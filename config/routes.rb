187f2a6030765d8e2b8a50db2a9a63460a278809
MuhammadTamzid <muhammad.tamzid@gmail.com>
2015-12-31 09:49:17 +0600

    static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vn/ do
+    root "static_pages#home"
+    get "home" => "static_pages#home"
+    get "about" => "static_pages#about"
+    get "contact" => "static_pages#contact"
   end
 end
