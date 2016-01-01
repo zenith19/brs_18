7d8ed1b018f94c649d2064ce3b581fe988d8886b
gittiggit <contactwithmahmud@gmail.com>
2016-01-01 11:40:55 +0600

    50090_edit_user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,17 @@
 class Admin::UsersController < ApplicationController
   before_action :authenticate_user!
+  load_and_authorize_resource
 
   def index
     @users = User.page(params[:page]).per 10
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t :delete_success_message
+      redirect_to admin_users_path
+    else
+      flash[:danger] = t :user_removal_failure
+    end
   end
 end
