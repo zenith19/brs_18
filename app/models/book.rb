4ef0d22cd3a0bd2e613d6f70b1a2cc753b6b3ab6
Sujoy Datta <sujoydatta25@yahoo.com>
2016-01-01 15:17:18 +0600

    delete book

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,14 +2,16 @@ class Book < ActiveRecord::Base
   belongs_to :category
   has_many :user_books
   has_many :reviews
-  has_many :book_photos
-
-  accepts_nested_attributes_for :book_photos, allow_destroy: true,
-    reject_if: proc {|attributes| attributes[:image].blank?}
+  has_many :book_photos, dependent: :destroy
 
   validates :title, presence: true, uniqueness: {case_sensitive: false}
   validates :author, presence: true
   validates :page, presence: true, numericality: {minimum: 1}
   validates :publish_date, presence: true
   validates :category, presence: true
+
+  accepts_nested_attributes_for :book_photos, allow_destroy: true,
+    reject_if: proc {|attributes| attributes[:image].blank?}
+
+  scope :latest, -> {order "created_at DESC"}
 end
