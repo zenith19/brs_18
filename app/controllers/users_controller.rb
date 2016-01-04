fd3d0e7abd973da2f05abe6594133b477a32eb9b
zenithit <zenith.it.ju@gmail.com>
2016-01-04 20:10:30 +0600

    50078_display_reading_history

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user_books = UserBook.by_user(current_user.id).page(params[:page]).per 10
   end
     
   def edit
--- app/models/user_book.rb
+++ app/models/user_book.rb
@@ -1,4 +1,7 @@
 class UserBook < ActiveRecord::Base
   belongs_to :user
   belongs_to :book
+  enum status: [:not_read, :reading, :finished]
+  
+  scope :by_user, ->user_id {where user_id: user_id}
 end
