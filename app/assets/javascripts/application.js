c3726ac58b18687f8f042643a5214f191badcb85
MuhammadTamzid <muhammad.tamzid@gmail.com>
2016-01-07 14:36:23 +0600

    ajax for rating and some design issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,10 +17,10 @@
 //= require turbolinks
 //= require_tree .
 
-$(document).ready(function() {
-$(".btn-pref .btn").click(function () {
+$(document).on('page:change', function() {
+  $(".btn-pref .btn").click(function () {
     $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
     // $(".tab").addClass("active"); // instead of this do the below
     $(this).removeClass("btn-default").addClass("btn-primary");
-});
-});
+  });
+})
