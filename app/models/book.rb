f135881a53e0d74593d43879f555b1eaa5b8b6e8
Tahmid Shakil <md.ahnaf.tahmid.shakil@framgia.com>
2016-01-12 15:37:18 +0600

    Feature - AdminBook Index [Search]

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,9 @@
 require "elasticsearch/model"
 
 class Book < ActiveRecord::Base
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
   belongs_to :category
   belongs_to :user
   has_many :user_books
@@ -29,27 +32,58 @@ class Book < ActiveRecord::Base
   scope :favourite, -> user_id{joins(:user_books)
                        .where user_books: {user_id: user_id, favourite: true}}
   scope :not_favourite, -> user_id{where.not id: [(favourite user_id).ids]}
+  #
+  # scope :filter_book, ->(criteria) do
+  #   search = criteria.split
+  #   case search[0]
+  #   when "all"
+  #     Book.all
+  #   when "unfavourite"
+  #     not_favourite search[1].to_i
+  #   else
+  #     favourite search[1].to_i
+  #   end
+  # end
 
-  scope :filter_book, ->(criteria) do
-    search = criteria.split
-    case search[0]
-    when "all"
-      Book.all
-    when "unfavourite"
-      not_favourite search[1].to_i
-    else
-      favourite search[1].to_i
+  class << self
+    def custom_search params
+      query=params[:query]
+      category=params[:category]
+      if category
+        self.__elasticsearch__.search(query: {match: {category_id: category}})
+          .records
+      elsif query
+        advanced = process_advanced query
+        if advanced
+          self.__elasticsearch__.search(query: {match:
+            {advanced[0] => advanced[1]}}).records
+        else
+          self.__elasticsearch__.search(query).records
+        end
+      else
+        self.all
+      end
+    end
+
+    def process_advanced query
+      if query =~ /^(title|rating):/
+        key = query.split ":"
+        key[1]=key[1].strip
+        key
+      end
     end
   end
 
   private
-  class << self
-    def ransackable_scopes auth_object = nil
-      [:filter_book]
-    end
-  end
+  # class << self
+  #   def ransackable_scopes auth_object = nil
+  #     [:filter_book]
+  #   end
+  # end
 
   def init_rating
     self.rating = 0 if self.rating.nil?
   end
 end
+
+Book.import
